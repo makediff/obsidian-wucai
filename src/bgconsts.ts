c18eac260febea5c0a141541535af959b460eb24
飞刀 <makediff@qq.com>
2023-03-23 14:35:15 +0800

    change tag

--- src/bgconsts.ts
+++ src/bgconsts.ts
@@ -3,7 +3,7 @@ export class BGCONSTS {
   static MARKET_INFO: string = 'obsidian-plugin'
   static ENDPOINT: string = 'obsidianwucai'
   static PRINT_LOG: boolean = false
-  static VERSION: string = '23.3.23'
+  static VERSION: string = '23.3.24'
   static VERSION_NUM: number = 230323
   static SERVICE_ID: number = 7
   static IS_DEBUG: boolean = false
