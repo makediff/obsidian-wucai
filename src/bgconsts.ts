ccd5dfd8ce1d97095a72afa56bed128a638e17c5
飞刀 <makediff@qq.com>
2023-09-24 20:56:58 +0800

    new version

--- src/bgconsts.ts
+++ src/bgconsts.ts
@@ -3,8 +3,8 @@ export class BGCONSTS {
   static SERVICE_ID: number = 7
   static MARKET_INFO: string = 'obsidian-plugin'
   static ENDPOINT: string = 'obsidianwucai'
-  static VERSION: string = '23.9.22'
-  static VERSION_NUM: number = 230922
+  static VERSION: string = '23.9.24'
+  static VERSION_NUM: number = 230924
   static IS_DEBUG: boolean = false
   static TEST_TOKEN: string = ''
   static BASE_URL: string = 'https://marker.dotalk.cn'
