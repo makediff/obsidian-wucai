18c48f5b7657f8625adc5dc5c1d5cb157f854f89
飞刀 <makediff@qq.com>
2023-10-20 14:51:16 +0800

    up

--- src/bgconsts.ts
+++ src/bgconsts.ts
@@ -5,7 +5,7 @@ export class BGCONSTS {
   static ENDPOINT: string = 'obsidianwucai'
   static VERSION: string = '23.10.20'
   static VERSION_NUM: number = 231020
-  static IS_DEBUG: boolean = true
+  static IS_DEBUG: boolean = false
   static TEST_TOKEN: string = ''
   static BASE_URL: string = 'https://marker.dotalk.cn'
   static OUT: string = 'json'
